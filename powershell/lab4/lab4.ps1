1d74899547be1160cc473bfc19e7b75e58d8f567
Yanfei <106874116+YanfeiWang3973@users.noreply.github.com>
2022-12-14 02:31:41 -0500

    Update lab4.ps1

--- powershell/lab4/lab4.ps1
+++ powershell/lab4/lab4.ps1
@@ -5,35 +5,18 @@ get-CimInstance -ClassName win32_computersystem | Format-List
 
 function get-osinfo {
 Write-Host "OS description:"
-get-CimInstance -ClassName win32_operatingsystem | Format-List Name, Version
+get-ciminstance win32_operatingsystem | select Name,Version | format-list
 }
 
 function get-CPU {
 Write-Host "Processor Information:"
-get-CimInstance -ClassName win32_processor | fl Description,
-MaxClockSpeed,
-NumberOfCores,
-@{n="L1CacheSize";e={switch($_.L1CacheSize){$Null{$myvar="data unavailable"; $myvar}};if($Null -ne $_.L1CacheSize){$_.L1CacheSize}}},
-@{n="L2CacheSize";e={switch($_.L2CacheSize){$Null{$myvar="data unavailable"; $myvar}};if($Null -ne $_.L2CacheSize){$_.L2CacheSize}}},
-@{n="L3CacheSize";e={switch($_.L3CacheSize){$Null{$myvar="data unavailable"; $myvar}};if($Null -ne $_.L3CacheSize){$_.L3CacheSize}}}
+	get-ciminstance win32_processor | select Description,NumberOfCores,MaxClockSpeed,L2CacheSize,L3CacheSize | format-list
 }
+
 
 function get-raminf {
 Write-Host "RAM infomation:"
-$Capacity = 0
-get-CimInstance -ClassName win32_physicalmemory |
-foreach {
-new-object -TypeName psobject -Property @{
-Manufacturer = $_.manufacturer
-Description = $_.Description
-"Size(GB)" = $_.capacity/1gb
-Bank = $_.Banklabel
-Slot = $_.devicelocator
-}
-$Capacity += $_.capacity/1gb
-} |
-Format-Table -auto Manufacturer, Description, "Size(GB)", Bank, Slot
-"Total RAM: ${Capacity}GB"
+	get-ciminstance win32_physicalmemory | select BankLabel,Description,DeviceLocator,Capacity,Manufacturer | format-table
 }
 
 function get-mydisks {
@@ -61,15 +44,9 @@ Foreach-Object {
 
 function get-networks {
 Write-Host "Network information:"
-get-ciminstance Win32_NetworkAdapterconfiguration |
-where {$_.ipenabled -eq "True" } | 
-Format-Table Description,
-Index,
-IPAddress,
-IPSubnet,
-@{n="DNSHostName";e={switch($_.DNSDomain){$Null{$myvar="data unavailable"; $myvar}};if($Null -ne $_.DNSDomain){$_.DNSDomain}}},
-@{n="DNSServerOrder";e={switch($_.DNSServersearchorder){$Null{$myvar="data unavailable"; $myvar}};if($Null -ne $_.DNSServersearchorder){$_.DNSServersearchorder}}}
+	get-ciminstance win32_networkadapterconfiguration | where {$_.IPEnabled -eq 'true'} | select Description,Index,IPAddress,IPSubnet,DNSDomain,DNSServerSearchOrder | ft
 }
+
 
 function get-gpu {
 Write-Host "GPU Information:"
